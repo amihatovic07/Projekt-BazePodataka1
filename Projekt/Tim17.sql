e37ec6ac38b28d4fbc41d9b75c49d6547ba9061e
amihatovic07 <alexmihatovic@gmail.com>
2026-04-26 20:46:18 +0200

    Commit2
    Ažurirana početna verzija tablica te je kreiran EER Dijagram koji služi za vizualizaciju projekta te za prikaz povezanosti pojedinih tablica sa sveukupnom strukturom sustava

--- Projekt/Tim17.sql
+++ Projekt/Tim17.sql
@@ -1,117 +1,159 @@
+DROP DATABASE IF EXISTS sustav_za_upravljanje_restoranom;
 CREATE DATABASE sustav_za_upravljanje_restoranom;
 USE sustav_za_upravljanje_restoranom;
 
 CREATE TABLE Zaposlenik (
-	id INT,
-    ime VARCHAR(30),
-    prezime VARCHAR(50),
-    datum_zaposlenja VARCHAR(10),
-    pozicija_zaposlenika VARCHAR(50),
-    placa_zaposlenika DECIMAL(6, 2)
+	id INTEGER PRIMARY KEY,
+    ime VARCHAR(30) NOT NULL,
+    prezime VARCHAR(50) NOT NULL,
+    datum_zaposlenja DATE NOT NULL,
+    pozicija_zaposlenika VARCHAR(50) NOT NULL,
+    placa_zaposlenika DECIMAL(6, 2) NOT NULL
 );
 
 CREATE TABLE Stol (
-	id INT,
-    broj_stola INT,
-    kapacitet_stola INT,
-    trenutna_zauzetost_stola BOOL
-);
-
-CREATE TABLE Jelo (
-	id INT,
-    naziv_jela VARCHAR(50),
-    cijena_jela DECIMAL(5, 2),
-    Trosak_pripravka_jela DECIMAL(5, 2)
+	id INTEGER PRIMARY KEY,
+    broj_stola INT NOT NULL UNIQUE,
+    kapacitet_stola INT NOT NULL,
+    trenutna_zauzetost_stola BOOL NOT NULL
 );
 
 CREATE TABLE Jelovnik (
-	id INT,
-    Jelo_id INT,
-    naziv_kategorije VARCHAR(40)
+	id INTEGER PRIMARY KEY,
+    naziv_kategorije VARCHAR(40) NOT NULL
 );
 
-CREATE TABLE Narudzbe (
-	id INT,
-    vrijeme_narudzbe VARCHAR(17),
-    Stol_id INT,
-    Zaposlenik_id INT
-);
-
-CREATE TABLE Stavka_Narudzbe (
-	id INT,
-    Narudzbe_id INT,
-    Jelo_ID INT,
-    kolicina INT
-);
-
-CREATE TABLE placanje (
-	id INT,
-    Narudzbe_id INT,
-    iznos DECIMAL(5, 2),
-    nacin_placanja VARCHAR(50)
+CREATE TABLE Jelo (
+	id INTEGER PRIMARY KEY,
+    naziv_jela VARCHAR(50) NOT NULL UNIQUE,
+    cijena_jela DECIMAL(5, 2) NOT NULL,
+    Trosak_pripravka_jela DECIMAL(5, 2) NOT NULL,
+    Jelovnik_id INTEGER NOT NULL,
+    FOREIGN KEY (Jelovnik_id) REFERENCES Jelovnik (id)
 );
 
 create table Specijalna_ponuda (
-	id INT,
-    Jelo_id INT,
-    ponuden_popust DECIMAL(5, 2),
-    pridodana_svojstva VARCHAR(40)
+	id INTEGER PRIMARY KEY,
+    Jelo_id INTEGER NOT NULL,
+    FOREIGN KEY (Jelo_id) REFERENCES Jelo (id),
+    ponuden_popust DECIMAL(5, 2) NOT NULL,
+    pridodana_svojstva VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE VIP_gosti (
-	id INT,
-    mjesecni_popust VARCHAR(3),
-    Specijalna_ponuda_id INT
+	id INTEGER PRIMARY KEY,
+    mjesecni_popust DECIMAL(4, 2) NOT NULL,
+    Specijalna_ponuda_id INTEGER NOT NULL,
+    FOREIGN KEY (Specijalna_ponuda_id) REFERENCES Specijalna_ponuda (id)
 );
 
 CREATE TABLE Kupac (
-	id INT,
-    ime VARCHAR(30),
-    prezime VARCHAR(50),
-    VIP_gosti_ID INT
+	id INTEGER PRIMARY KEY,
+    ime VARCHAR(30) NOT NULL,
+    prezime VARCHAR(50) NOT NULL,
+    VIP_gosti_id INTEGER NULL,
+    FOREIGN KEY (VIP_gosti_id) REFERENCES VIP_gosti (id)
+);
+
+CREATE TABLE Narudzbe (
+	id INTEGER PRIMARY KEY,
+    vrijeme_narudzbe DATETIME NOT NULL,
+    Stol_id INTEGER NOT NULL,
+    FOREIGN KEY (Stol_id) REFERENCES Stol (id),
+    Kupac_id INTEGER NOT NULL,
+    FOREIGN KEY (Kupac_id) REFERENCES Kupac (id),
+    Zaposlenik_id INTEGER NOT NULL,
+    FOREIGN KEY (Zaposlenik_id) REFERENCES Zaposlenik (id)
+);
+
+CREATE TABLE Stavka_Narudzbe (
+	id INTEGER PRIMARY KEY,
+    Narudzbe_id INTEGER NOT NULL,
+    FOREIGN KEY (Narudzbe_id) REFERENCES Narudzbe (id),
+    Jelo_id INTEGER NOT NULL,
+    FOREIGN KEY (Jelo_id) REFERENCES Jelo (id),
+    kolicina INTEGER NOT NULL
+);
+
+CREATE TABLE Placanje (
+	id INTEGER PRIMARY KEY,
+    Narudzbe_id INTEGER NOT NULL,
+    FOREIGN KEY (Narudzbe_id) REFERENCES Narudzbe (id),
+    iznos DECIMAL(5, 2) NOT NULL,
+    nacin_placanja VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Rezervacije (
-	id INT,
-    Stol_id INT,
-    vrijeme_rezervacije VARCHAR(17),
-    Kupac_id INT
+	id INTEGER PRIMARY KEY,
+    Stol_id INTEGER NOT NULL,
+    FOREIGN KEY (Stol_id) REFERENCES Stol (id),
+    vrijeme_rezervacije DATETIME NOT NULL,
+    Kupac_id INTEGER NOT NULL,
+    FOREIGN KEY (Kupac_id) REFERENCES Kupac (id)
 );
 
 CREATE TABLE Dostava (
-	id INT,
-    Kupac_id int,
-    Zaposlenik_id INT,
-    placanje_id INT,
-    vrijeme_dostave VARCHAR(17)
+	id INTEGER PRIMARY KEY,
+    Kupac_id INTEGER NOT NULL,
+    FOREIGN KEY (Kupac_id) REFERENCES Kupac (id),
+    Zaposlenik_id INTEGER NOT NULL,
+    FOREIGN KEY (Zaposlenik_id) REFERENCES Zaposlenik (id),
+    Narudzbe_id INTEGER  NOT NULL,
+    FOREIGN KEY (Narudzbe_id) REFERENCES Narudzbe (id),
+    vrijeme_dostave DATETIME NOT NULL
 );
 
 CREATE TABLE Racuni_prihodi (
-	id INT,
-    iznos_racuna DECIMAL(6,2),
-    vrijeme_izdavanja_racuna VARCHAR(17),
-    status_racuna VARCHAR(20),
-    Placanje_id INT
-);
-
-CREATE TABLE Racuni_rashodi (
-	id INT,
-    vrsta_rashoda VARCHAR(50),
-    vrijeme_izdavanja_racuna VARCHAR(17),
-    iznos DECIMAL(6,2),
-    status_racuna VARCHAR(20)
+	id INTEGER PRIMARY KEY,
+    iznos_racuna DECIMAL(6,2) NOT NULL,
+    vrijeme_izdavanja_racuna DATETIME NOT NULL,
+    status_racuna VARCHAR(20) NOT NULL,
+    Placanje_id INTEGER NOT NULL,
+    FOREIGN KEY (Placanje_id) REFERENCES Placanje (id)
 );
 
 CREATE TABLE Resursi (
-	id INT,
-    naziv VARCHAR(50),
-    kolicina_resursa INT,
-    vrijednost_resursa DECIMAL(6, 2)
+	id INTEGER PRIMARY KEY,
+    naziv VARCHAR(50) NOT NULL,
+    kolicina_resursa INT NOT NULL,
+    vrijednost_resursa DECIMAL(6, 2) NOT NULL
+);
+
+CREATE TABLE Nabava_resursi (
+	id INTEGER PRIMARY KEY,
+    Resursi_id INTEGER NOT NULL,
+    FOREIGN KEY (Resursi_id) REFERENCES Resursi (id),
+    cijena DECIMAL(6, 2) NOT NULL,
+    kolicina INTEGER NOT NULL,
+    datum_nabave DATE NOT NULL
+);
+
+CREATE TABLE Racuni_rashodi (
+	id INTEGER PRIMARY KEY,
+    vrsta_rashoda VARCHAR(50) NOT NULL,
+    vrijeme_izdavanja_racuna DATETIME NOT NULL,
+    Nabava_resursi_id INTEGER NOT NULL,
+    FOREIGN KEY (Nabava_resursi_id) REFERENCES Nabava_resursi (id),
+    iznos DECIMAL(6,2) NOT NULL,
+    status_racuna VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Obracun_prihoda_i_rashoda (
-	id INT,
-    Racuni_prihodi_id INT,
-    Racuni_rashodi_id INT,
-    konacni_iznos DECIMAL(6,2)
+	id INTEGER PRIMARY KEY,
+    Racuni_prihodi_id INTEGER NOT NULL,
+    FOREIGN KEY (Racuni_prihodi_id) REFERENCES Racuni_prihodi (id),
+    Racuni_rashodi_id INTEGER NOT NULL,
+    FOREIGN KEY (Racuni_rashodi_id) REFERENCES Racuni_rashodi (id),
+    konacni_iznos DECIMAL(6,2) NOT NULL
+);
+
+CREATE TABLE Bilanca (
+	id INTEGER PRIMARY KEY,
+	Obracun_prihoda_i_rashoda_id INTEGER NOT NULL,
+    FOREIGN KEY (Obracun_prihoda_i_rashoda_id) REFERENCES Obracun_prihoda_i_rashoda (ID),
+	Resursi_id INTEGER NOT NULL,
+    FOREIGN KEY (Resursi_id) REFERENCES Resursi (id),
+    stanje_prije DECIMAL(8,2) NOT NULL,
+    stanje_poslije DECIMAL(8,2) NOT NULL,
+    datum_bilance DATE NOT NULL
 );
